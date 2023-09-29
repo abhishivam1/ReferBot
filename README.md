b5ca6c41d0196695d5db8230a07e42d5c182ec59
Abhishek Mishra <95407551+abhishivam1@users.noreply.github.com>
2023-09-29 20:38:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ReferBot
-refer bot mde by reeshu for newbie
+# Newbie Refer Bot 
+- For personal use of @its_newbie....
+
+# Deploy Button
+- [![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template={{repository_url}})
+
+
+## made by @reeshuxd
---
+++ main.go
@@ -0,0 +1,140 @@
+// Made by @reeshuxd
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"newbi/db"
+	"strconv"
+	"time"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+)
+
+var token = `6695071556:AAE_27WV_GDeBt1-lJk5BGSaTMgK4VnglTU` // Your Bot token here i.e. by @botfather....
+
+func main() {
+
+	b, err := gotgbot.NewBot(token, &gotgbot.BotOpts{
+		BotClient: &gotgbot.BaseBotClient{
+			Client: http.Client{},
+			DefaultRequestOpts: &gotgbot.RequestOpts{
+				Timeout: gotgbot.DefaultTimeout,
+				APIURL:  gotgbot.DefaultAPIURL,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	updater := ext.NewUpdater(&ext.UpdaterOpts{
+		Dispatcher: ext.NewDispatcher(&ext.DispatcherOpts{
+			// If an error is returned by a handler, log it and continue going.
+			Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+				log.Println("an error occurred while handling update:", err.Error())
+				return ext.DispatcherActionNoop
+			},
+			MaxRoutines: ext.DefaultMaxRoutines,
+		}),
+	})
+
+	dispatcher := updater.Dispatcher
+	dispatcher.AddHandler(handlers.NewCommand("start", Start))
+	dispatcher.AddHandler(handlers.NewCommand("join", Join))
+	dispatcher.AddHandler(handlers.NewCommand("competition", Competiton))
+	dispatcher.AddHandler(handlers.NewCommand("referral", Referral))
+
+	err = updater.StartPolling(b, &ext.PollingOpts{
+		DropPendingUpdates: true,
+		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
+			Timeout: 9,
+			RequestOpts: &gotgbot.RequestOpts{
+				Timeout: time.Second * 10,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+
+	fmt.Printf("%s has been started...\n", b.User.Username)
+	updater.Idle()
+}
+
+func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
+	db.AddUser(ctx.EffectiveSender.Id())
+	message := ctx.EffectiveMessage
+	var user int64
+
+	if len(ctx.Args()) != 1 {
+		user, _ = strconv.ParseInt(ctx.Args()[1], 10, 64)
+		if db.CheckUser(ctx.EffectiveSender.Id()) {
+			message.Reply(bot, "You have already starteed the bot!", nil)
+			return nil
+		}
+		db.Refer_Update(user, "e") // Updating user refers....
+		message.Reply(bot, fmt.Sprintf("YOu are invited by %v", user), nil)
+	} else {
+		user = 908732147
+		message.Reply(bot, "Bot is alive", nil)
+	}
+	return nil
+}
+
+func Join(bot *gotgbot.Bot, ctx *ext.Context) error {
+	message := ctx.EffectiveMessage
+	if ctx.EffectiveChat.Type != "supergroup" && ctx.EffectiveChat.Type != "group" {
+		message.Reply(bot, "This bot can only be used in groups", nil)
+		return nil
+	}
+
+	text := `
+Hello %s, Here is your referral link:
+Link - https://t.me/%s?start=%v
+	`
+	message.Reply(bot, fmt.Sprintf(text, ctx.EffectiveSender.FirstName(), bot.Username, ctx.EffectiveSender.Id()), nil)
+	return nil
+}
+
+func Competiton(bot *gotgbot.Bot, ctx *ext.Context) error {
+	message := ctx.EffectiveMessage
+	if ctx.EffectiveChat.Type != "supergroup" && ctx.EffectiveChat.Type != "group" {
+		message.Reply(bot, "This bot can only be used in groups", nil)
+		return nil
+	}
+	ctx.EffectiveMessage.Reply(bot, "Prize Money is 200rs!", nil)
+	return nil
+}
+
+func Referral(bot *gotgbot.Bot, ctx *ext.Context) error {
+	message := ctx.EffectiveMessage
+	if ctx.EffectiveChat.Type != "supergroup" && ctx.EffectiveChat.Type != "group" {
+		message.Reply(bot, "This bot can only be used in groups", nil)
+		return nil
+	}
+
+	users, err := db.GetUsersByRefersAscending()
+	if err != nil {
+		message.Reply(bot, err.Error(), nil)
+		return nil
+	}
+
+	text := "Top Refers:\n"
+	for _, user := range users {
+		text += fmt.Sprintf("%d - %d refers\n", user.UserID, user.Refers)
+	}
+
+	message.Reply(bot, text, nil)
+	return nil
+}
+
+type User struct {
+	UserID int64 `bson:"user_id"`
+	Refers int64 `bson:"refers"`
+}
+
+// Made by @reeshuxd
